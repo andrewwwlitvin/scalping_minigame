ae71eecb675348870222795dfebc87b22dc40945
andrewwwlitvin <andrewwwlitvin@gmail.com>
2025-09-17 23:48:50 +0100

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+declare global {
+  interface Window { Telegram?: any }
+}
+
+const tg = window.Telegram?.WebApp;
+tg?.ready?.();
+tg?.expand?.(); // use full height if allowed
+
+createRoot(document.getElementById("root")!).render(<App />);
